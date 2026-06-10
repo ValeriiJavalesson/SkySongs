b8a8ed46c200728f82f393ac337aac8e4c28458d
VALERKO <valerava1@i.ua>
2026-06-10 14:10:13 +0300

    Revert "Додавання файлів до проекту"
    This reverts commit acaf5ab47106cbb4036f08ce74c3b836608e2605.

--- src/main/java/com/pysarivka/WeekEnds/dao/DayRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pysarivka.WeekEnds.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pysarivka.WeekEnds.domain.Day;
-
-public interface DayRepository extends JpaRepository<Day, Long>{
-
-}
